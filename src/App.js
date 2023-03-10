e27fe7e1422568068723b00de1154879e44c0f83
BertanMortas <bertanmortas@gmail.com>
2023-03-10 15:41:45 +0300

    degisimler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { Search } from "./search";
-import { ConcertAdd } from "./ConcertAdd";
+import { Search } from "./Components/search";
+import { ConcertAdd } from "./Components/ConcertAdd";
 import { useContext, useEffect } from "react";
-import ConcertContext from "./concerts";
-
+import ConcertContext from "./Components/concerts";
 
 function App() {
-const {fetchConcerts,concerts} = useContext(ConcertContext)
+  const { fetchConcerts, concerts } = useContext(ConcertContext);
 
-  useEffect(()=>{
-    fetchConcerts()
-  },[])
+  useEffect(() => {
+    fetchConcerts();
+  }, []);
 
   return (
-    <div >
-     <h1>hello world</h1>
-     <h1>concert app</h1>
-     <Search details={concerts} />
-     
-     <ConcertAdd />
-     
+    <div>
+      <h1>hello world</h1>
+      <h1>concert app</h1>
+      <Search details={concerts} />
+
+      <ConcertAdd />
     </div>
-    
   );
 }
 
